a6a8dc16d182d4f5866ebe8ab42004a094735ded
MK-Raihan <mkraihan207@gmail.com>
2022-04-22 22:49:05 +0600

    pricing from server

---
+++ src/Pages/Home/Pricing/Price/Price.js
@@ -0,0 +1,70 @@
+import React from 'react';
+
+const Price = (props) => {
+    const {name, price, free, disk, data, email, uplink} = props.price
+    return (
+        <div className="col" data-aos="fade-right"
+                        data-aos-easing="linear"
+                        data-aos-duration="1000">
+                <div className="card h-100 pricing-card">
+                <div className="pricing-header p-3 text-start mb-3">
+                <div className="card-icon w-25 mx-auto">
+                <i className="fab fa-ioxhost"></i>
+                </div>
+                <div className="card-body ">
+                    <h2 className="card-title">{name}</h2>
+                    <h6 className="card-title">Start at:</h6>
+                    <h5 className="card-title">$<span className='fs-1'>{price}</span> /month</h5>
+                    <strong> {free}</strong> 
+                </div>
+                </div>
+                <div className="option">
+                        <ul>
+                            <li className='list-unstyled d-flex align-items-center mb-4'>
+                                <div className="option-icon">
+                                <i className="fas fa-compact-disc"></i>
+                                </div>
+                                <div className="option-content ms-4 text-start">
+                                    <h6>DISK</h6>
+                                    <h6>{disk}</h6>
+                                </div>
+                            </li>
+                            <li className='list-unstyled d-flex align-items-center mb-4'>
+                                <div className="option-icon">
+                                <i className="fas fa-database"></i>
+                                </div>
+                                <div className="option-content ms-4 text-start">
+                                    <h6>DATA</h6>
+                                    <h6>{data}</h6>
+                                </div>
+                            </li>
+                            <li className='list-unstyled d-flex align-items-center mb-4'>
+                                <div className="option-icon">
+                                <i className="far fa-envelope"></i>
+                                </div>
+                                <div className="option-content ms-4 text-start">
+                                    <h6>EMAIL</h6>
+                                    <h6>{email}</h6>
+                                </div>
+                            </li>
+                            <li className='list-unstyled d-flex align-items-center mb-4'>
+                                <div className="option-icon">
+                                <i className="fas fa-igloo"></i>
+                                </div>
+                                <div className="option-content ms-4 text-start">
+                                    <h6>TLD</h6>
+                                    <h6>{uplink}</h6>
+                                </div>
+                            </li>
+                            
+                        </ul>
+                    </div>
+                    <div className="pricing-btn py-3">
+                    <button className='btn order-btn'>Order Now</button>
+                    </div>
+                </div>
+            </div>
+    );
+};
+
+export default Price;
--- src/Pages/Home/Pricing/Pricing.js
+++ src/Pages/Home/Pricing/Pricing.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
+import Price from './Price/Price';
 import './Pricing.css'
 
+
 const Pricing = () => {
+    const [pricing, setPricing] = useState([]);
+
+    useEffect(()=>{
+        fetch('http://localhost:5000/pricing')
+        .then(res=>res.json())
+        .then(data=>setPricing(data))
+    },[])
+
     return (
         <div className='py-5 text-white pricing-main'>
             <div className="container">
@@ -9,191 +19,12 @@ const Pricing = () => {
                     <h1>Our Hosting Plan</h1>
                 </div>
             <div className="row row-cols-1 row-cols-lg-3 row-cols-md-2 g-4">
-
-            <div className="col" data-aos="fade-right"
-                        data-aos-easing="linear"
-                        data-aos-duration="1000">
-                <div className="card h-100 pricing-card">
-                <div className="pricing-header p-3 text-start mb-3">
-                <div className="card-icon w-25 mx-auto">
-                <i className="fab fa-ioxhost"></i>
-                </div>
-                <div className="card-body ">
-                    <h2 className="card-title">Shared Hosting</h2>
-                    <h6 className="card-title">Start at:</h6>
-                    <h5 className="card-title">$<span className='fs-1'>8.19</span> /month</h5>
-                    <strong> Web Hosting With Free Domain</strong> 
-                </div>
-                </div>
-                <div className="option">
-                        <ul>
-                            <li className='list-unstyled d-flex align-items-center mb-4'>
-                                <div className="option-icon">
-                                <i className="fas fa-compact-disc"></i>
-                                </div>
-                                <div className="option-content ms-4 text-start">
-                                    <h6>DISK</h6>
-                                    <h6>250GB SSD</h6>
-                                </div>
-                            </li>
-                            <li className='list-unstyled d-flex align-items-center mb-4'>
-                                <div className="option-icon">
-                                <i className="fas fa-database"></i>
-                                </div>
-                                <div className="option-content ms-4 text-start">
-                                    <h6>DATA</h6>
-                                    <h6>1TB BANDWIDTH</h6>
-                                </div>
-                            </li>
-                            <li className='list-unstyled d-flex align-items-center mb-4'>
-                                <div className="option-icon">
-                                <i className="far fa-envelope"></i>
-                                </div>
-                                <div className="option-content ms-4 text-start">
-                                    <h6>EMAIL</h6>
-                                    <h6>120 EMAIL</h6>
-                                </div>
-                            </li>
-                            <li className='list-unstyled d-flex align-items-center mb-4'>
-                                <div className="option-icon">
-                                <i className="fas fa-igloo"></i>
-                                </div>
-                                <div className="option-content ms-4 text-start">
-                                    <h6>TLD</h6>
-                                    <h6>30 DOMAINS</h6>
-                                </div>
-                            </li>
-                            
-                        </ul>
-                    </div>
-                    <div className="pricing-btn py-3">
-                    <button className='btn order-btn'>Order Now</button>
-                    </div>
-                </div>
-            </div>
-            <div className="col" data-aos="fade-up"
-                        data-aos-easing="linear"
-                        data-aos-duration="1000">
-                <div className="card h-100 pricing-card">
-                <div className="pricing-header p-3 text-start mb-3">
-                <div className="card-icon w-25 mx-auto">
-                <i className="fas fa-server"></i>
-                </div>
-                <div className="card-body ">
-                    <h2 className="card-title">Dedicated Server</h2>
-                    <h6 className="card-title">annually get (20% discount)</h6>
-                    <h5 className="card-title">$<span className='fs-1'>82.00</span> /month</h5>
-                    <strong> Web Hosting Without Free Domain</strong> 
-                </div>
-                </div>
-                <div className="option">
-                        <ul>
-                            <li className='list-unstyled d-flex align-items-center mb-4'>
-                                <div className="option-icon">
-                                <i className="fas fa-compact-disc"></i>
-                                </div>
-                                <div className="option-content ms-4 text-start">
-                                    <h6>CPU</h6>
-                                    <h6>4x 3.20Ghz 2 Cores</h6>
-                                </div>
-                            </li>
-                            <li className='list-unstyled d-flex align-items-center mb-4'>
-                                <div className="option-icon">
-                                <i className="fas fa-database"></i>
-                                </div>
-                                <div className="option-content ms-4 text-start">
-                                    <h6>RAM</h6>
-                                    <h6>16GB (up to 32GB)</h6>
-                                </div>
-                            </li>
-                            <li className='list-unstyled d-flex align-items-center mb-4'>
-                                <div className="option-icon">
-                                <i className="far fa-envelope"></i>
-                                </div>
-                                <div className="option-content ms-4 text-start">
-                                    <h6>DRIVES</h6>
-                                    <h6>2 x 1TB SATA 3.5</h6>
-                                </div>
-                            </li>
-                            <li className='list-unstyled d-flex align-items-center mb-4'>
-                                <div className="option-icon">
-                                <i className="fas fa-igloo"></i>
-                                </div>
-                                <div className="option-content ms-4 text-start">
-                                    <h6>UPLINK</h6>
-                                    <h6>1Gbps - 20TB</h6>
-                                </div>
-                            </li>
-                            
-                        </ul>
-                    </div>
-                    <div className="pricing-btn py-3">
-                    <button className='btn order-btn'>Order Now</button>
-                    </div>
-                </div>
-            </div>
-            <div className="col" data-aos="fade-left"
-                        data-aos-easing="linear"
-                        data-aos-duration="1000">
-                <div className="card h-100 pricing-card">
-                <div className="pricing-header p-3 text-start mb-3">
-                <div className="card-icon w-25 mx-auto">
-                <i className="fas fa-cloud-download-alt"></i>
-                </div>
-                <div className="card-body ">
-                    <h2 className="card-title">Shared Hosting</h2>
-                    <h6 className="card-title">Start at:</h6>
-                    <h5 className="card-title">$<span className='fs-1'>8.19</span> /month</h5>
-                    <strong> Web Hosting With Free Domain</strong> 
-                </div>
-                </div>
-                <div className="option">
-                        <ul>
-                            <li className='list-unstyled d-flex align-items-center mb-4'>
-                                <div className="option-icon">
-                                <i className="fas fa-compact-disc"></i>
-                                </div>
-                                <div className="option-content ms-4 text-start">
-                                    <h6>DISK</h6>
-                                    <h6>250GB SSD</h6>
-                                </div>
-                            </li>
-                            <li className='list-unstyled d-flex align-items-center mb-4'>
-                                <div className="option-icon">
-                                <i className="fas fa-database"></i>
-                                </div>
-                                <div className="option-content ms-4 text-start">
-                                    <h6>DATA</h6>
-                                    <h6>1TB BANDWIDTH</h6>
-                                </div>
-                            </li>
-                            <li className='list-unstyled d-flex align-items-center mb-4'>
-                                <div className="option-icon">
-                                <i className="far fa-envelope"></i>
-                                </div>
-                                <div className="option-content ms-4 text-start">
-                                    <h6>EMAIL</h6>
-                                    <h6>120 EMAIL</h6>
-                                </div>
-                            </li>
-                            <li className='list-unstyled d-flex align-items-center mb-4'>
-                                <div className="option-icon">
-                                <i className="fas fa-igloo"></i>
-                                </div>
-                                <div className="option-content ms-4 text-start">
-                                    <h6>TLD</h6>
-                                    <h6>30 DOMAINS</h6>
-                                </div>
-                            </li>
-                            
-                        </ul>
-                    </div>
-                    <div className="pricing-btn py-3">
-                    <button className='btn order-btn'>Order Now</button>
-                    </div>
-                </div>
-            </div>
-           
+                {
+                    pricing.map(price=><Price 
+                        key={price._id}
+                        price={price}
+                    ></Price>)
+                }
             </div>
             </div>
         </div>
